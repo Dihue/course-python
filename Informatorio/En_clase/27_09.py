715ccaec1e8b29a1eb119cd278cde8fda1f460fd
Dihue <dihue21@gmail.com>
2021-09-27 16:05:22 -0300

    First class

--- Informatorio/En_clase/27_09.py
+++ Informatorio/En_clase/27_09.py
@@ -1,1 +1,15 @@
-27_09.py
+# Solucionando problemas de instalacion
+
+print("Me estoy durmiendo")
+
+# Variables
+
+num1 = 10
+num2 = 20
+
+suma = num1 + num2
+
+# f es para format o formato (lo que viene a continuacion
+# tiene un formato especial) y escribe el conternido
+# de la variable entre llaves
+print(f"La suma de {num1} y {num2} es {suma}")
