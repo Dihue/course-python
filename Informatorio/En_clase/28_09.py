611c9159d15bca9bdf34c3327880e27864f4d1c7
Dihue <dihue21@gmail.com>
2021-09-28 16:00:52 -0300

    Second class

---
+++ Informatorio/En_clase/28_09.py
@@ -0,0 +1,35 @@
+"""
+variable: espacio en memoria (identificado con nombre) ej: x = 5
+
+asignacion: accion por la cual guardo valor en una variable
+
+accion ingreso/salida: mostrar por pantalla print() --- input()
+
+escructuras de control
+	condicionales (if, elif, else)
+	repetitivas (while, for)
+
+> Sistema de Informacion
+
+	Entrada --> Proceso --> Salida
+"""
+"""
+x = 4
+y = 'A'
+r = str(x) + y
+
+print(r)
+
+edad = 30
+nombre = 'Dihue'
+
+print(f"Hola, mi nombre es {nombre} y tengo {edad} años")
+
+apellido = input("Ingregar apellido:")
+print(f"Y el apellido es {apellido}")
+"""
+print("\n--------------------\n")
+num1 = int(input("Ingresar primer numero: "))
+num2 = int(input("Ingresar segundo numero: "))
+
+print(f"\nResultado de la suma es: {num1 + num2}")
