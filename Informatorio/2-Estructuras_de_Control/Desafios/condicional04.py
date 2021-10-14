030387db5bfff1919e779870c2806dbaeb4d4e50
Dihue <dihue21@gmail.com>
2021-10-14 00:32:11 -0300

    Exercise resolved

--- Informatorio/2-Estructuras_de_Control/Desafios/condicional04.py
+++ Informatorio/2-Estructuras_de_Control/Desafios/condicional04.py
@@ -16,3 +16,36 @@ de receta. Al final se debe mostrar por pantalla la receta ele-
 gida y todos los ingredientes.
 '''
 
+receta = input("\nSeleccione la receta que desea:\n 1-Lentejas y apio\n 2-Morrón y cebolla\n-> ")
+
+if receta == '1':
+	ingredientes = "lentejas y apio"
+	print("\nSeleccione ingrediente extra para su receta")
+	comun = input("\n 1-Verduras\n 2-Berenjena\n 3-Nada\n-> ")
+
+	if comun == '1':
+		extra = "verduras"
+		print(f"\nSeleccón: Receta 1 de {ingredientes} con {extra}")
+
+	elif comun == '2':
+		extra = "berenjena"
+		print(f"\nSeleccón: Receta 1 de {ingredientes} con {extra}")
+
+	else:
+		print(f"\nSeleccón: Receta 1 de {ingredientes}")
+
+elif receta == '2':
+	ingredientes = "morrón y cebolla"
+	print("\nSeleccione ingrediente extra para su receta")
+	comun = input("\n 1-Verduras\n 2-Berenjena\n 3-Nada\n-> ")
+
+	if comun == '1':
+		extra = "verduras"
+		print(f"\nSeleccón: Receta 2 de {ingredientes} con {extra}")
+
+	elif comun == '2':
+		extra = "berenjena"
+		print(f"\nSeleccón: Receta 2 de {ingredientes} con {extra}")
+
+	else:
+		print(f"\nSeleccón: Receta 2 de {ingredientes}")
