c6af4c88eaca8566a9721940e9e0e55261fdb985
Dihue <dihue21@gmail.com>
2021-10-18 20:51:48 -0300

    Class

---
+++ Informatorio/En_clase/18_10.py
@@ -0,0 +1,12 @@
+# Funciones:
+
+def suma(x,y):
+	r = x + y
+	return r
+
+num1 = int(input("Nº 1: "))
+num2 = int(input("Nº 2: "))
+
+resultado = suma(num1,num2)
+
+print(f"El resultado es {resultado}")
