a8fd1c8174214f18f42368b5461f4b1261c69b63
Dihue <dihue21@gmail.com>
2021-11-22 19:40:19 -0300

    Completed exercise

--- Informatorio/5-POO/01_vs_programacion_estructurada/estructurada.py
+++ Informatorio/5-POO/01_vs_programacion_estructurada/estructurada.py
@@ -4,3 +4,41 @@ clientes = [
     {'Nombre':'Hector','Apellidos':'Costas Guzman','DNI':'11223344A'},
     {'Nombre':'Juan','Apellidos':'Gonzales Marquez','DNI':'55667788A'}
 ]
+
+
+# Creamos una funcion que muestra un cliente en una lista a partir del DNI
+
+def mostrar_clientes(clientes,dni):
+    for c in clientes:
+        if (dni == c['DNI']):
+            print('{} {}'.format(c['Nombre'],c['Apellidos']))
+            return
+    print('Cliente no encontrado')
+
+
+# Creamos una funcion que borra un cliente en una lista a partir del DNI
+
+def borrar_cliente(clientes,dni):
+    for i,c in enumerate(clientes):
+        if (dni == c['DNI']):
+            del(clientes[i])
+            print(str(c),'> BORRADO')
+            return
+    print('Cliente no entontrado')
+
+
+# Observa muy bien como se utiliza el codigo estructurado
+
+print('\n---- Listado de Clientes ----')
+print(clientes)
+
+print('\n|-> Mostrar clientes por DNI:')
+mostrar_clientes(clientes,'11223344A')
+mostrar_clientes(clientes,'55667788A')
+
+print('\n|-> Borrar clientes por DNI:')
+borrar_cliente(clientes,'11223344A')
+borrar_cliente(clientes,'55667788A')
+
+print('\n---- Listado de Clientes ----')
+print(clientes)
--- Informatorio/5-POO/01_vs_programacion_estructurada/orientada_objetos.py
+++ Informatorio/5-POO/01_vs_programacion_estructurada/orientada_objetos.py
@@ -7,5 +7,57 @@ class Clientes:
         self.nombre = nombre
         self.apellido = apellido
 
-    def __str__(self) -> str:
-        return '{} {}'.format(self.nombre,self.apellido)
+    def __str__(self):
+        return f'{self.nombre} {self.apellido}'
+
+
+# Creo una estructura para las empresas
+
+class Empresa:
+
+    def __init__(self,clientes=[]):
+        self.clientes = clientes
+
+    def mostrar_clientes(self,dni=None):
+        for c in self.clientes:
+            if c.dni == dni:
+                print(c)
+                return
+        print('Cliente no encontrado')
+
+    def borrar_cliente(self,dni=None):
+        for i,c in enumerate(self.clientes):
+            if c.dni == dni:
+                del (self.clientes[i])
+                print(str(c),">Borrado")
+                return
+        print('Cliente no entontrado')
+
+
+# Ahora utilizaremos ambas estructuras
+
+# Creamos un par de clientes
+hector = Clientes('123123123A','Hector','Toledo')
+denis = Clientes('321321321B','Denis','Nuñez')
+
+# Creamos una empresa con los clientes inciales
+empresa = Empresa(clientes=[hector,denis])
+
+# Se muestran los clientes
+print('\n|--> Lista de Clientes:')
+print(empresa.clientes)
+
+# Se muestran clientes por DNI
+print('\n|--> Mostrar Clientes por DNI:')
+# Se consulta clientes por DNI
+empresa.mostrar_clientes('123123123A')
+empresa.mostrar_clientes('321321321B')
+
+print('\n|--> Borrar Clientes por DNI:')
+# Se borra clientes por DNI
+empresa.borrar_cliente('123123123A')
+empresa.borrar_cliente('321321321B')
+
+# Se muestran de nuevo los clientes
+print('\n|--> Lista de Clientes:')
+print(empresa.clientes)
